0a887e9d5372fb7c3b098e3e639a4bd719bc2b41
Jiyoung <thdwldud8@gmail.com>
2021-07-28 17:46:18 +0900

    [지영] - 장고 삭제

--- 지영/Smoreproject/account/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-class User(AbstractUser):
-    company_representative = models.CharField(max_length=6)
-    company_name = models.CharField(max_length=50)
-    company_number = models.CharField(max_length=10)
-    company_email = models.EmailField(max_length=128)
