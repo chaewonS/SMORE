6509c8d7d578286d3f8d71bf1d8d8f8d6959ab0a
Seung-a-oh <osa0215@naver.com>
2021-07-31 23:20:14 +0900

    [승아] - 메인 페이지 이미지 버튼 추가, 슬라이드 레이아웃 구성

--- Seung-A/app/src/main/java/org/techtown/smore/MainActivity.java
+++ Seung-A/app/src/main/java/org/techtown/smore/MainActivity.java
@@ -3,6 +3,7 @@ package org.techtown.smore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
